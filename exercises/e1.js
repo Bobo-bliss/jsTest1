c10bf8ee4b660c06837080921642aeaaae5b612e
Rob Schultz <Robschultz4444@yahoo.com>
2023-03-10 19:36:38 -0600

    Completed exercises

--- exercises/e1.js
+++ exercises/e1.js
@@ -14,9 +14,9 @@
 */
 
 // Your code goes here
-
-
-
+function logMessage() {
+    console.log('Hello, World!')
+}
 
 /**
  * =============================================================
@@ -30,9 +30,9 @@
 */
 
 // Your code goes here...
-
-
-
+function logMyMessage(str) {
+    console.log(str)
+}
 
 /**
  * ===============================================================
@@ -45,9 +45,9 @@
 */
 
 // Your code goes here...
-
-
-
+function logMessageWithMyName(name) {
+    console.log('My name is ' + name + '!')
+}
 
 /**
  * ==============================================================
@@ -61,7 +61,9 @@
 */
 
 // Your code goes here...
-
+function logMyInfo(name, age) {
+    console.log('My name is ' + name + '. I am ' + age + ' years old!')
+}
 
 
 
--- exercises/e4.js
+++ exercises/e4.js
@@ -9,8 +9,8 @@
  * splitFirstAndLastNames('John Smith') => ['John', 'Smith']
  */
 
-function splitFirstAndLastNames(str) {
-  return str.split(' ');
+const splitFirstAndLastNames = (str) => {
+    return str.split(' ');
 }
 
 
@@ -20,9 +20,9 @@ function splitFirstAndLastNames(str) {
  * personAge({ id: 1, name: 'Someone', age: 32}) => 32
  */
 
-const personAge = function(personObject) {
-  return personObject.age;
-}
+const personAge = (personObject) => {
+    return personObject.age;
+  }
 
 
 /** =========================
@@ -32,9 +32,7 @@ const personAge = function(personObject) {
  * isNameInArray(['Jon', 'Michael', 'Andrey'], 'James') => false;
  */
 
-const isNameInArray = function(arr, name) {
-  return arr.includes(name);
-}
+const isNameInArray = (arr, name) => arr.includes(name);
 
 
 /** =========================
@@ -44,16 +42,16 @@ const isNameInArray = function(arr, name) {
  * !!! Hint: There are two functions to refactor !!!
  */
 
-const logSecondsUpToMax = function(max) {
-  let i = 0;
-  const timer = setInterval(function() {
-    if (i < max) {
-      console.log(++i);
-    } else {
-      clearInterval(timer);
-    }
-  }, 1000);
-}
+const logSecondsUpToMax = (max) => {
+    let i = 0;
+    const timer = setInterval(() => {
+      if (i < max) {
+        console.log(++i);
+      } else {
+        clearInterval(timer);
+      }
+    }, 1000);
+  }
 
 
 
